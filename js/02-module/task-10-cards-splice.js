67a3e13915dc9da79fad63aa8ccb650eb6789ba6
Stanislav Karmanov <106772749+AlexanderIvakhnenko@users.noreply.github.com>
2022-07-31 13:23:53 +0300

    add

--- js/02-module/task-10-cards-splice.js
+++ js/02-module/task-10-cards-splice.js
@@ -1,60 +1,93 @@
 
-/*
- * Работем с коллекцией карточек в trello
- * - Метод splice()
- * - Удалить
- * - Добавить
- * - Обновить
- */
+// /*
+//  * Работем с коллекцией карточек в trello
+//  * - Метод splice()
+//  * - Удалить
+//  * - Добавить
+//  * - Обновить
+//  */
 
-const cards = [
-  'Карточка-1',
-  'Карточка-2',
-  'Карточка-3',
-  'Карточка-4',
-  'Карточка-5',
-];
+// const cards = [
+//   'Карточка-1',
+//   'Карточка-2',
+//   'Карточка-3',
+//   'Карточка-4',
+//   'Карточка-5',
+// ];
+
+// // console.table(cards);
+
+// /*
+//  * Удаление (по индексу), метод indexOf()
+//  */
+
+// const cardToRemove = 'Карточка-3';
+
+// // узнаем индекс элемента
+// const index = cards.indexOf(cardToRemove);
+// console.table(index);
+
+// // удаляем элемент из массива
+// // cards.splice(с какого номера индекса, сколько удалить элементов с индекс позиции);
+// cards.splice(index, 1);
 
 // console.table(cards);
 
-/*
- * Удаление (по индексу), метод indexOf()
- */
+// /*
+//  * Добавление (по индексу)
+//  */
 
-const cardToRemove = 'Карточка-3';
+// const cardToInsert1 = 'Карточка-ADD-1';
+// const cardToInsert2 = 'Карточка-ADD-2';
 
-// узнаем индекс элемента
-const index = cards.indexOf(cardToRemove);
-console.table(index);
+// cards.splice(index, 0, cardToInsert1, cardToInsert2 );
 
-// удаляем элемент из массива
-// cards.splice(с какого номера индекса, сколько удалить элементов с индекс позиции);
-cards.splice(index, 1);
+// console.table(cards);
 
-console.table(cards);
+// /*
+//  * Обновление (по индексу)
+//  */
 
-/*
- * Добавление (по индексу)
- */
+// const cardToUpdate = 'Карточка-Update';
 
-const cardToInsert1 = 'Карточка-ADD-1';
-const cardToInsert2 = 'Карточка-ADD-2';
+// // удаление с заменой
+// cards.splice(3, 1 , cardToUpdate);
 
-cards.splice(index, 0, cardToInsert1, cardToInsert2 );
-
-console.table(cards);
-
-/*
- * Обновление (по индексу)
- */
-
-const cardToUpdate = 'Карточка-Update';
-
-// удаление с заменой
-cards.splice(3, 1 , cardToUpdate);
-
-console.table(cards);
+// console.table(cards);
 
 
-const clients = ["Mango", "Ajax", "Poly", "Kiwi", "Bingo"];
-console.log(clients.slice(1, 2));
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi", "Bingo"];
+// console.log(clients.slice(1, 2));
+
+const array = ['Earth', 'Mars', 'Venus'];
+//   // Change code below this line
+// let firstAndLast;
+//    firstAndLast = `${array[0]}, ${array[array.length-1]}`;
+//   // Change code above this line
+
+// console.log(firstAndLast);
+
+
+
+//   // Change code below this line
+// let firstAndLast;
+// firstAndLast = `${array[0]}, ${array[array.length-1]}`;
+//   // Change code above this line
+// console.log(firstAndLast);
+
+function getExtremeElements(array) {
+  // Change code below this line
+
+ const firstAndLastEL=array.splice(0, 3  )
+ 
+ 
+
+  // console.log(firstEl); 
+  console.log(firstAndLastEL); 
+   
+  // return firstAndLast;
+  // Change code above this line
+}
+// getExtremeElements([1, 2, 3, 4, 5]);
+// getExtremeElements(["Earth", "Mars", "Venus"]);
+getExtremeElements(["apple", "peach", "pear", "banana"]);
---
+++ js/03-module-function/06-change-case.js
@@ -0,0 +1,35 @@
+
+/*
+ * Напиши функцию changeCase(string) которая заменяет регистр
+ * каждого символа в строке на противоположный.
+ * Например, если строка «JavaScript», то на выходе должена быть строка «jAVAsCRIPT».
+ */
+
+const changeCase = function (string) {
+    
+    const letters = string.split(" ");
+    const invertString = '';
+
+    if (lettet === letters.toLowerCase()) {
+        
+    }
+    
+}
+
+console.log(changeCase('qweRTY')); // QWErty
+console.log(changeCase('mAnGo')); // MaNgO
+console.log(changeCase('AjAx')); // aJaX
+
+
+    // if (letter === letter.toLocaleLowerCase()) {
+//         // перебираем нижний регистр
+//         console.log('В нижнем регистре', letter);
+//         // перезаписываем нижний в верхний
+//         invertedString  += letter.toLocaleUpperCase();
+//     } else {
+//          // перебираем верхний регистр
+//         console.log('В верхнем регистре', letter);
+//          // перезаписываем нижний в нижний
+//          invertedString  += letter.toLocaleLowerCase();
+//     }
+// }
